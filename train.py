1d7a028a8842144a2eccff068b7946825e902f61
SharonSwarnil <166638548+SharonSwarnil@users.noreply.github.com>
2026-04-15 15:12:37 +0530

    Initial commit: Robotics perception & depth estimation project

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
-from ultralytics import YOLO
 import os
+import torch
+from ultralytics import YOLO
 
 def train_model():
-
     print("Loading YOLOv8 Nano model for edge-optimized training...")
-
     model = YOLO("yolov8n.pt")
 
     DATASET_YAML_PATH = "dataset/data.yaml"
@@ -20,7 +19,8 @@ def train_model():
         epochs=10, # Adjust as needed for better results. Mine is CPU so it takes more time, That's why I set it to just 10 epoches
         batch=10,
         name="robot_nav_v1",
-        device="cpu" # Use 0 for GPU if available. Mine is CPU-only, so I set it to "cpu". Adjust as your configuration   
+        device="cpu", # Use 0 for GPU if available. Mine is CPU-only, so I set it to "cpu". Adjust as your configuration 
+        workers=0 # Set to 0 because Windows sometimes freezes during multi-threading on CPU
     )
 
     print("\nTraining finished.")
@@ -28,13 +28,18 @@ def train_model():
 
     print("\nExporting optimized ONNX model for edge devices...")
 
+    # Automatically check for GPU to avoid forcing FP16 quantization on a CPU
+    is_gpu = torch.cuda.is_available()
+    
     exported_path = model.export(
         format="onnx",
-        half=True
+        half=is_gpu
     )
 
-    print(f"Optimized model saved at: {exported_path}")
-
+    if is_gpu:
+        print(f"Optimized FP16 ONNX model saved at: {exported_path}")
+    else:
+        print(f"Standard FP32 ONNX model saved at: {exported_path} (CPU detected)")
 
 if __name__ == "__main__":
     train_model()
